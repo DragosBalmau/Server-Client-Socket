8475577077341eb9cbc00491ddaf935652315c4b
DragosBalmau <balmaudragos@gmail.com>
2020-10-20 16:46:31 +0300

    Update

--- server1.c
+++ server1.c
@@ -8,16 +8,11 @@
 #define IP "127.0.0.1"
 #define PORT 8080
 #define SIZE 1024
-#define NUM_CLIENT 1
 
 struct thread_data_t {
 
-    pthread_t thread;
-    int tid;
-    int server_sock;
-    int new_sock;
-    struct sockaddr_in server_addr, new_addr;
-    socklen_t addr_size;
+    int new_socket;
+    struct sockaddr_in new_addr;
 
 };
 
@@ -58,8 +53,6 @@ void* receiveDataBinary(void* p_sockfd){
 
     int sockfd = *((int *)p_sockfd);
 
-    printf("TEST\n");
-
     FILE *fileOutput = fopen("data.out", "wb");
 
     char buffer[SIZE];
@@ -67,8 +60,6 @@ void* receiveDataBinary(void* p_sockfd){
     int dimFileInput = 0, dimFileOutput = 0;
 
     recvAll(sockfd, &dimFileInput, sizeof(dimFileInput), sizeof(int));
-
-    printf("%d\n", dimFileInput);
 
     int dataReceived = 0;
     
@@ -80,8 +71,6 @@ void* receiveDataBinary(void* p_sockfd){
 
         fwrite(buffer, 1, sizeof(buffer[0]) * dataReceived, fileOutput);
 
-        //printf("%s\n", buffer);
-
         if(dimFileOutput == dimFileInput)
             return NULL;
 
@@ -89,47 +78,49 @@ void* receiveDataBinary(void* p_sockfd){
     }
 
     close(sockfd);
+    pthread_exit(0);
     fclose(fileOutput);
+   
     return NULL;
 
 }
 
 int main(){
 
-    struct thread_data_t thr_data[NUM_CLIENT];
+    struct thread_data_t pthread_arg;
+    pthread_t thread;
+    int server_sock;
 
+    struct sockaddr_in server_addr;
+    socklen_t addr_size;
     int opt = 1;
-    
-    for (int i = 0; i < NUM_CLIENT; ++i) {
 
-        thr_data[i].tid = i;
-        check(thr_data[i].server_sock = socket(AF_INET, SOCK_STREAM, 0), "[-]Socket failed");
-        printf("[+]Server socket created successfully on thread %d.\n", thr_data[i].tid);
+    check(server_sock = socket(AF_INET, SOCK_STREAM, 0), "[-]Socket failed");
+    printf("[+]Server socket created successfully.\n");
 
-        thr_data[i].server_addr.sin_family = AF_INET;
-        thr_data[i].server_addr.sin_port = PORT;
-        thr_data[i].server_addr.sin_addr.s_addr = inet_addr(IP);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = PORT;
+    server_addr.sin_addr.s_addr = inet_addr(IP);
 
-        setsockopt(thr_data[i].server_sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
-        check(bind(thr_data[i].server_sock, (struct sockaddr*)&(thr_data[i].server_addr), sizeof(thr_data[i].server_addr)),"[-]Bind failed");
-        printf("[+]Binding successfull.\n");
+    check(bind(server_sock, (struct sockaddr*)&(server_addr), sizeof(server_addr)),"[-]Bind failed");
+    printf("[+]Binding successfull.\n");
 
-        check(listen(thr_data[i].server_sock , 3) == 0,"[-]Listen failed");
-        printf("[+]Listening....\n");
+    check(listen(server_sock , 50) == 0,"[-]Listen failed");
+    printf("[+]Listening....\n");
 
-        thr_data[i].addr_size = sizeof(thr_data[i].new_addr);
-        check(thr_data[i].new_sock = accept(thr_data[i].server_sock, (struct sockaddr*)&(thr_data[i].new_addr), &(thr_data[i].addr_size)),"[-]Accept failed");
+    while(1){
 
-        pthread_create(&(thr_data[i].thread), NULL, receiveDataBinary, &(thr_data[i].server_sock));
-        
-        //close(thr_data[i].server_sock);
-    }
+	    addr_size = sizeof(pthread_arg.new_addr);
 
-    printf("[+]Data written in the file successfully.\n");
+	    check(pthread_arg.new_socket = accept(server_sock, (struct sockaddr*)&pthread_arg.new_addr, &addr_size),"[-]Accept failed");
 
-    for (int i = 0; i < NUM_CLIENT; ++i) //block pana termina fiecare thread
-        pthread_join(thr_data[i].thread, NULL);
+	    pthread_create(&thread, NULL, receiveDataBinary, &pthread_arg.new_socket);
+	  	
+	    printf("[+]Data written in the file successfully with thread\n");
+
+	}
 
     return 0;
 }
